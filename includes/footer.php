c97cd11d683a893615b86a5d2cb395c668996c8b
motlj <joshuadamotl@gmail.com>
2016-02-29 17:51:14 -0600

    Created many pages, first attempt at checking functionality

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+
+  <nav class="navbar navbar-default navbar-fixed-bottom">
+    <div class="row">
+      <div class="container">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+          <center class="emphasis">&reg;2016</center>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+          <center class="emphasis">&copy;Josh Motl 2016</center>
+        </div>
+      </div>
+    </div>
+  </nav>
