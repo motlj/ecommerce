54a5d2084ebb2d628130dd68f6be0aaa1ad32eba
motlj <joshuadamotl@gmail.com>
2016-02-29 21:21:34 -0600

    attempting different block of code for login

--- includes/login.php
+++ includes/login.php
@@ -17,68 +17,60 @@
 require_once('database.php');
 require_once('navbar.php');
 
-$error=''; 
-if (isset($_POST['login'])) {
-	if (empty($_POST['user_name']) || empty($_POST['password'])) {
-		$error = "Username or Password is invalid";
-	} else {
-		$user_name=$_POST['user_name'];
-		$password=$_POST['password'];
-		$connection = mysql_connect("localhost", "root", "");
-		$user_name = stripslashes($user_name);
-		$password = stripslashes($password);
-		$user_name = mysql_real_escape_string($user_name);
-		$password = mysql_real_escape_string($password);
-		$db = mysql_select_db("ecommerce", $connection);
-		$query = mysql_query("SELECT * FROM customer where password='$password' AND user_name='$user_name'", $connection);
-		$rows = mysql_num_rows($query);
-		if ($rows == 1) {
-			$_SESSION['login_user']=$user_name;
-			header("location: ../index.php");
-		} else {
+
+
+	session_start(); // Starting Session
+	$error=''; // Variable To Store Error Message
+
+	if (isset($_POST['submit'])) {
+		if (empty($_POST['user_name']) || empty($_POST['password'])) {
 			$error = "Username or Password is invalid";
+		} else {
+			$user_name=$_POST['user_name'];
+			$password=$_POST['password'];
+			//$connection = mysql_connect("localhost", "root", "");
+			$user_name = stripslashes($user_name);
+			$password = stripslashes($password);
+			$user_name = mysql_real_escape_string($user_name);
+			$password = mysql_real_escape_string($password);
+			//$db = mysql_select_db("ecommerce", $connection);
+			$query = mysql_query("SELECT * FROM customer where password='$password' AND user_name='$user_name'"/*, $connection*/);
+			$rows = mysql_num_rows($query);
+			if ($rows == 1) {
+				$_SESSION['login_user']=$user_name; 
+				header("location: ../index.php"); // Redirecting To Other Page
+			} else {
+				$error = "Username or Password is invalid";
+			}
+			//mysql_close($connection); // Closing Connection
 		}
-		mysql_close($connection);
 	}
-}
-
-
-
-/*	$user_name = $_POST['user_name']; 
-	$password = $_POST['password']; 
-
-	function SignIn() { 
-		session_start(); //starting the session for user profile page 
-		if(!empty($_POST['user'])) { 
-			$query = mysql_query("SELECT * FROM customer where user_name = '$_POST[user_name]' AND password = '$_POST[password]'") or die(mysql_error()); 
-			$row = mysql_fetch_array($query) or die(mysql_error()); 
-			if(!empty($row['user_name']) AND !empty($row['password'])) { 
-				$_SESSION['user_name'] = $row['password']; 
-				echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE..."; 
-			} else { 
-				echo "SORRY... YOU ENTERED WRONG ID AND PASSWORD... PLEASE RETRY..."; 
-			} 
-		} 
-	} 
-	
-	if(isset($_POST['submit'])) { 
-	SignIn(); 
-	} 
 
 
 
 
-	if(!empty($_POST['user_name'])&&isset($_POST['user_name'])){
+
+
+
+
+
+
+/*	if(!empty($_POST['user_name'])&&isset($_POST['user_name'])){
 		if(!empty($_POST['password'])&&isset($_POST['password'])){
 			$_SESSION['uid'] = ""
 			$_SESSION['user_name']=$_POST['user_name'];
 			header('Location: ../index.php');
 		}
-	
-
-
-header('Location: ../index.php');
+	}
+	header('Location: ../index.php');
 */
+
+
+
+
+
+
+
 
 	require_once('footer.php');
 ?>
