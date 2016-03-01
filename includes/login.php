8790abbd92680d318e9db2ba0e1349195c7aa2d5
motlj <joshuadamotl@gmail.com>
2016-02-29 22:41:41 -0600

    added php tag to logout.php & updated login to more clearly verify username and password

--- includes/login.php
+++ includes/login.php
@@ -1,60 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
-  </head>
-  <body>
-    <br>
-    <br>
-    <br>
-    <br>
-
 <?php
-require_once('navbar.php');
 require_once('session.php');
 
-
-
-
-
-	if(!empty($_POST['user_name'])&&isset($_POST['user_name'])){
-		if(!empty($_POST['password'])&&isset($_POST['password'])){
-			$_SESSION['uid'] = ""
-			$_SESSION['user_name']=$_POST['user_name'];
-			//header('Location: ../index.php');
+	if(!empty($_POST['user_name']) && isset($_POST['user_name'])){
+		if(!empty($_POST['password']) && isset($_POST['password'])){
+			$username=$_POST['user_name'];
+			$password=$_POST['password'];
 			try { 
 				$pdo = Database::connect();
 			    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			    $sql = "SELECT * FROM customer WHERE password='$password' AND user_name='$user_name'");
+			    $sql = "SELECT * FROM customer WHERE user_name=?");
 			    $q = $pdo->prepare($sql);
+           		$q->execute(array($username));
 			    Database::disconnect();
-			    //header("Location: ../index.php");
+			    // check if user was found and
+			    // check if password from db = $password; if it is the same
+				if ($username = ($_POST['user_name']) && $password = ($_POST['password'])) {
+				    $_SESSION['uid'] = 5;
+				    $_SESSION['permissions'] = 2;
+				    $_SESSION['name'] = "bob jones";
+				    header('Location: ../index.php'); // successfully logged in
+			    } else {
+					header('Location: ../index.php'); // username/password pair not found
+				}
 			} catch (PDOException $e) { 
            		echo "Syntax Error: ".$e->getMessage(); 
            		die();
-       			}
 			}
 		}
 	}
-	//header('Location: ../index.php');
-
-
-
-
-
-
-
-
-
-	require_once('footer.php');
-?>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>  
-
-  </body>
-</html>
+	header('Location: ../index.php'); // accessed page incorrectly, redirecting
