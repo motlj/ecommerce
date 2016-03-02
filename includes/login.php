b78ab4086f8447b74726dc75e771795b6d33506d
motlj <joshuadamotl@gmail.com>
2016-03-01 18:46:25 -0600

    made more changes to login.php in an attempt to work

--- includes/login.php
+++ includes/login.php
@@ -8,22 +8,27 @@ require_once('session.php');
 			try { 
 				$pdo = Database::connect();
 			    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			    $sql = "SELECT * FROM customer WHERE user_name=?");
+			    $sql = "SELECT * FROM customer WHERE user_name = ? AND $password = ?");
 			    $q = $pdo->prepare($sql);
            		$q->execute(array($username));
 			    Database::disconnect();
 			    // check if user was found and
 			    // check if password from db = $password; if it is the same
 				if ($username = ($_POST['user_name']) && $password = ($_POST['password'])) {
-				    $_SESSION['uid'] = 5;
-				    $_SESSION['permissions'] = 2;
-				    $_SESSION['name'] = "bob jones";
-				    echo "You have successfully logged in."
-				    header('Location: ../index.php'); // successfully logged in
-			    } else {
+				    echo "You have successfully logged in. Welcome back" . $_SESSION['name'];
+				} else {
 			    	echo "Username/Password pair not recognized.";
 					header('Location: ../index.php'); // username/password pair not found
 				}
+
+				session_start();
+				    $_SESSION['uid'] = $id;
+				    $_SESSION['name'] = $name;
+				    $_SESSION['last_name'] = $last_name;
+				    $_SESSION['user_name'] = $user_name;
+				    $_SESSION['permissions'] = $permission;
+
+				    //header('Location: ../index.php'); // successfully logged in     				}
 			} catch (PDOException $e) { 
            		echo "Syntax Error: ".$e->getMessage(); 
            		die();
