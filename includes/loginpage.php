e4b14c1d47cc1bf10fb9976ceea9808c980b28e3
motlj <joshuadamotl@gmail.com>
2016-03-01 19:11:30 -0600

    fixed loginpage.php had no closing html tags

--- includes/loginpage.php
+++ includes/loginpage.php
@@ -39,3 +39,12 @@
           <input type="submit" value="Register">
         </form>
       </div>
+    </div>
+    
+    <?php require_once('includes/footer.php');?>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>  
+
+  </body>
+</html>
