e87e8efcdd06f28b33e9a6f6c543c2edef52b50b
motlj <joshuadamotl@gmail.com>
2016-03-01 22:53:34 -0600

    added html tags to head.php

--- includes/head.php
+++ includes/head.php
@@ -1,8 +1,11 @@
-<head>
+<!DOCTYPE html>
+
+ <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!--   <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">   -->
     <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
-</head>
+ </head>
+</html>
