f5f1be2974633d57136143376641c6f0eb6f72dd
motlj <joshuadamotl@gmail.com>
2016-03-01 21:24:16 -0600

    changed if statement in session.php

--- includes/session.php
+++ includes/session.php
@@ -1,7 +1,19 @@
-	require_once('database.php');
-	session_start();
+<?php
+
+/*session_start();
 	
 	$loggedin = false
 	if (!empty($_SESSION['user_name'])) {
 		$loggedin = true;
 	}
+
+*/
+	
+session_start();
+	
+	if( isset( $_SESSION['user_name'])) {
+	      echo "you are logged in" ;
+	   } else {
+	   	  echo "create account to login";
+	   }
+?>
