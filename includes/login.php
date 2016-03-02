a2d20ad40be0b093d61b44f53269410df4fedb4b
motlj <joshuadamotl@gmail.com>
2016-03-01 19:38:01 -0600

    commented out some parts of login.php

--- includes/login.php
+++ includes/login.php
@@ -8,42 +8,42 @@ require_once('session.php');
 			$username=$_POST['user_name'];
 			$loginpassword=$_POST['password'];
 
-			try { 
-			    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			    $sql = "SELECT * FROM customer WHERE user_name = ? AND $password = ?";
-			    $q = $pdo->prepare($sql);
-           		$q->query(array($username,$password));
-           		$query = $q->fetch(PDO::FETCH_ASSOC);
-           		$name = $query['name'];
-           		$last_name = $query['last_name'];
-           		$user_name = $query['user_name'];
-           		$password = $query['password'];
-           		$id = $query['id'];
-           		$permission = $query['permission'];
-			    Database::disconnect();
+	//try { 
+		    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		    $sql = "SELECT * FROM customer WHERE user_name = ? AND $password = ?";
+		    $q = $pdo->prepare($sql);
+       		$q->query(array($username,$password));
+       		$query = $q->fetch(PDO::FETCH_ASSOC);
+       		$name = $query['name'];
+       		$last_name = $query['last_name'];
+       		$user_name = $query['user_name'];
+       		$password = $query['password'];
+       		$id = $query['id'];
+       		$permission = $query['permission'];
+		    Database::disconnect();
 
 			    //session_start();
-				$_SESSION['id'] = $id;
-				$_SESSION['name'] = $name;
-				$_SESSION['last_name'] = $last_name;
-				$_SESSION['user_name'] = $user_name;
-				$_SESSION['permission'] = $permission;
+			$_SESSION['id'] = $id;
+			$_SESSION['name'] = $name;
+			$_SESSION['last_name'] = $last_name;
+			$_SESSION['user_name'] = $user_name;
+			$_SESSION['permission'] = $permission;
 
 
 			    // check if user was found and
 			    // check if password from db = $password; if it is the same
-				if ($username == $user_name && $loginpassword == $password) {
-				    echo "You have successfully logged in. Welcome back," . $_SESSION['name'];
-				} else {
-			    	echo "Username/Password pair not recognized.";
-					//header('Location: ../index.php'); // username/password pair not found
-				}
+			if ($username == $user_name && $loginpassword == $password) {
+			    echo "You have successfully logged in. Welcome back," . $_SESSION['name'];
+			} else {
+		    	echo "Username/Password pair not recognized.";
+				//header('Location: ../index.php'); // username/password pair not found
+			}
 
 				    //header('Location: ../index.php'); // successfully logged in     				}
-			} catch (PDOException $e) { 
-           		echo "Syntax Error: ".$e->getMessage(); 
-           		die();
-			}
+		//	} catch (PDOException $e) { 
+          // 		echo "Syntax Error: ".$e->getMessage(); 
+           	//	die();
+			
 		}
 	}
 	header('Location: ../index.php'); // accessed page incorrectly, redirecting
