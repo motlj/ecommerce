ec276909f1e5a6a788087038cedf8e335f4e2c57
motlj <joshuadamotl@gmail.com>
2016-03-01 23:00:41 -0600

    fixed links to css and js

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!--   <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">   -->
-    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 
     <title>Ecommerce</title>
  </head>
@@ -59,7 +59,7 @@
     <?php require_once('includes/footer.php');?>
 
 
-  <script src="../assets/js/jquery.min.js"></script>
-  <script src="../assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
   </body>
 </html>
