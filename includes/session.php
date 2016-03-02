3bf51ee122f144b474763e8b4bd200651c8cd10a
motlj <joshuadamotl@gmail.com>
2016-03-01 21:25:26 -0600

    swapped out commented section in session.php

--- includes/session.php
+++ includes/session.php
@@ -1,19 +1,19 @@
 <?php
 
-/*session_start();
+session_start();
 	
 	$loggedin = false
 	if (!empty($_SESSION['user_name'])) {
 		$loggedin = true;
 	}
 
-*/
+
 	
-session_start();
+/*session_start();
 	
 	if( isset( $_SESSION['user_name'])) {
 	      echo "you are logged in" ;
 	   } else {
 	   	  echo "create account to login";
 	   }
-?>
+*/?>
