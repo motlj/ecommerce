2a1e289ba83c9c5605cb490c9bee8922f3f5c79a
motlj <joshuadamotl@gmail.com>
2016-03-01 20:58:23 -0600

    debugged and removed if statements from login.php

--- includes/login.php
+++ includes/login.php
@@ -15,14 +15,13 @@ require_once('database.php');
 		    $q = $pdo->prepare($sql);
        		$q->execute(array($username,$loginpassword));
        		$query = $q->fetch(PDO::FETCH_ASSOC);
+		    Database::disconnect();
 
-       		print_r($query);
-/*       		$name = $query['name'];
+       		//print_r($query);
+       		$name = $query['name'];
        		$user_name = $query['user_name'];
-       		$password = $query['password'];
        		$id = $query['id'];
        		$permission = $query['permission'];
-		    Database::disconnect();
 
 			session_start();
 			$_SESSION['name'] = $name;
@@ -30,16 +29,15 @@ require_once('database.php');
 			$_SESSION['id'] = $id;
 			$_SESSION['permission'] = $permission;
 
-
-	if(!empty($_POST['user_name']) && isset($_POST['user_name'])){
-		if(!empty($_POST['password']) && isset($_POST['password'])){
-			if ($username == $user_name && $loginpassword == $password) {
+/*			if ($username == $user_name && $loginpassword == $password) {
 			    echo "You have successfully logged in. Welcome back," . $_SESSION['name'] . "We've been waiting for you.";
 			} else {
 		    	echo "Username/Password pair not recognized.";
 			}*/
-			//header('Location: ../index.php');			
+
+
+			header('Location: ../index.php');			
 		}
 	}
-	//header('Location: ../index.php'); 
+	header('Location: ../loginpage.php'); 
 ?>
