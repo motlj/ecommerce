f0af01844b65f074e0bdbe78c0c0e0d6338c4b06
motlj <joshuadamotl@gmail.com>
2016-03-02 17:55:59 -0600

    commented out duplicate lines in update

--- update.php
+++ update.php
@@ -6,8 +6,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">  
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+<!--     <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">  
+ -->    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 
     <title>Ecommerce</title>
   </head>
@@ -50,8 +50,8 @@
               $q->execute(array($username));
               $query = $q->fetch(PDO::FETCH_ASSOC);
 
-              $q = $pdo->prepare($sql);
-              $q->execute(array($username));
+              //$q = $pdo->prepare($sql);
+              //$q->execute(array($username));
               foreach ($pdo->query($query) as $row) {
                 echo '<tr>';
 
@@ -76,7 +76,7 @@
               }
                 
               Database::disconnect();
-              //print_r($query);
+              print_r($query);
           }
           ?>
         </tbody>
