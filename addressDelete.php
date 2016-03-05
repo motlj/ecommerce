29f3b09ac9d9fc22e1aa24e47fa99b0908fd7fb6
motlj <joshuadamotl@gmail.com>
2016-03-05 12:32:29 -0600

    changes SQL query in delete to reference customer_address table instead of address table

--- addressDelete.php
+++ addressDelete.php
@@ -7,7 +7,7 @@
         $id = $_POST['id'];
         $pdo = Database::connect();
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "DELETE FROM `ecommerce`.`address` WHERE `address`.`id` = ?"; //taken from SQL query on phpMyAdmin
+        $sql = "DELETE FROM `ecommerce`.`customer_address` WHERE `address_fk` = ?"; //taken from SQL query on phpMyAdmin
         $q = $pdo->prepare($sql);
         $q->execute(array($id));
         Database::disconnect();
