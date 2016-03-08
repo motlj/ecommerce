565b0fe72b17a2c966b6803802b6e530f794aaea
motlj <joshuadamotl@gmail.com>
2016-03-07 20:34:31 -0600

    changed AND to WHERE

--- ccCreate.php
+++ ccCreate.php
@@ -153,7 +153,7 @@ require_once 'includes/database.php';
             try {
               $pdo = Database::connect();
               $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-              $sql = "SELECT * FROM address LEFT JOIN customer_address ON (address.id=customer_address.address_fk) AND (customer_address.customer_fk = ". $_SESSION['id'] . ")";
+              $sql = "SELECT * FROM address LEFT JOIN customer_address ON (address.id=customer_address.address_fk) WHERE (customer_address.customer_fk = ". $_SESSION['id'] . ")";
               $address = $pdo->query($sql);
               echo "<select name='Address'>";
               foreach ($address as $row) {
