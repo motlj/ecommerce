ab47100a5172ef28016a34d926540826224c7402
motlj <joshuadamotl@gmail.com>
2016-03-07 18:58:31 -0600

    added correct $address_fk to post values

--- ccCreate.php
+++ ccCreate.php
@@ -17,8 +17,9 @@ require_once 'includes/database.php';
       $card_number = $_POST['card_number'];
       $expiration = $_POST['expiration'];
       $security = $_POST['security'];
-      $address_fk = $POST['$row['id']']
-         
+      $address_fk = $_POST['Address'];
+
+
         // validate input
       $valid = true;
         
@@ -161,7 +162,7 @@ require_once 'includes/database.php';
               // $q->fetchAll());
               echo "<select name='Address'>";
               foreach ($address as $row) {
-                echo "<option value='" . $row['id'] . "'>" . $row['street1'] . "</option>";
+                echo "<option name='Address' value='" . $row['id'] . "'>" . $row['street1'] . "</option>";
               }
               echo "</select>";
               Database::disconnect();
