7b4118b569ef30bc0902b1049cf60a5ed668ae5d
motlj <joshuadamotl@gmail.com>
2016-03-07 21:47:43 -0600

    put navbar on all pages

--- ccCreate.php
+++ ccCreate.php
@@ -77,15 +77,14 @@ require_once 'includes/database.php';
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!--   <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">   -->
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">  
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 
     <title>Add a Credit Card</title>
  </head>
   
   <body>
-    <?php // require_once('includes/navbar.php');?>
+    <?php require_once('includes/navbar.php');?>
 
 
     <div class="container">
