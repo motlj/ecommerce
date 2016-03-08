f273554d35f1e4d7922d49c215cfffc94c4e84aa
motlj <joshuadamotl@gmail.com>
2016-03-07 19:13:57 -0600

    added print_r for debugging

--- ccCreate.php
+++ ccCreate.php
@@ -47,6 +47,7 @@ require_once 'includes/database.php';
         // insert data
       if ($valid) {
         try {
+          print_r($address_fk);
           $pdo = Database::connect();
           $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           $sql = "INSERT INTO credit_card (type,name,card_number,expiration,security,address_fk) values(?, ?, ?, ?, ?, ?)";
