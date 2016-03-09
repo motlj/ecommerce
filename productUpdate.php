d7418a719e94bf939bda59d9dafb6a32cdab04e0
motlj <joshuadamotl@gmail.com>
2016-03-08 20:07:32 -0600

    added category and bin fks to allow updating

--- productUpdate.php
+++ productUpdate.php
@@ -9,20 +9,22 @@
       $product_name = $_POST['product_name'];
       $description = $_POST['description'];
       $price = $_POST['price'];
+      $category_fk = $_POST['category_fk'];
+      $bin_fk = $_POST['bin_fk']
          
       function valid($varname){
         return ( !empty($varname) && isset($varname) );
       }
 
-      if (!valid($product_name) || !valid($description) || !valid($price)) {
+      if (!valid($product_name) || !valid($description) || !valid($price) || !valid($category_fk) || !valid($bin_fk)) {
         header("Location: adminUpdate.php");
       }
 
       $pdo = Database::connect();
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "UPDATE category SET product_name = ?, description = ?, price = ? WHERE id = ?";
+        $sql = "UPDATE category SET product_name = ?, description = ?, price = ?, category_fk = ?, bin_fk = ? WHERE id = ?";
         $q = $pdo->prepare($sql);
-        $q->execute(array($name,$description,$price,$id));
+        $q->execute(array($name,$description,$price,$category_fk,$bin_fk,$id));
       Database::disconnect();
       header("Location: adminUpdate.php");
     }
