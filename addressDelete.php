c90c8196883636935c1aff02c0fd5b68af0e0951
motlj <joshuadamotl@gmail.com>
2016-03-09 20:58:01 -0600

    fixed delete

--- addressDelete.php
+++ addressDelete.php
@@ -1,26 +1,11 @@
 <?php
     require_once 'includes/session.php';
     require_once 'includes/database.php';
+    require_once 'includes/crud.php';
  
-    if ( !empty($_POST['id']) && isset($_POST['id'])) {
-/*      try { 
-        $id = $_POST['id'];
-        $pdo = Database::connect();
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "DELETE FROM `ecommerce`.`customer_address` WHERE `address_fk` = ?"; //taken from SQL query on phpMyAdmin
-        $q = $pdo->prepare($sql);
-        $q->execute(array($id));
-        Database::disconnect();
-        header("Location: update.php");
-      } catch (PDOException $e) { 
-        echo "Syntax Error: ".$e->getMessage() . "<br />\n"; 
-        header("Location: update.php?error=1");
-      }
-*/
-      $addressID = $_POST['id'];
+    if ( !empty($_POST){
 
       $deleteAddress = new customerAddress($_SESSION['id']);
-      $deleteAddress->delete($addressID);
-
+      $deleteAddress->delete($_POST['id']);
 
     }
