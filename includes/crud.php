2fe9d0c37b79b05276d986db8333bce06daf0d63
motlj <joshuadamotl@gmail.com>
2016-03-09 21:41:00 -0600

    fixed variable names

--- includes/crud.php
+++ includes/crud.php
@@ -7,13 +7,8 @@ function valid($varname){
 
 //customer table crud functions
 class customer {	
-	public $customer_id;
-	
-	public function __construct($customer_id){
-		$this->customer_id = $customer_id;
-	}
-	
-/*	public function create($street1, $street2, $city, $state, $zip, $country){
+
+	public function create($street1, $street2, $city, $state, $zip, $country){
 		if (!valid($street1) || !valid($street2) || !valid($city) || !valid($state) || !valid($zip) || !valid($country)) {
 			return false;
 		} else {
@@ -21,21 +16,17 @@ class customer {
 			$sql = "INSERT INTO address (street1,street2,city,state,zip,country) values(?, ?, ?, ?, ?, ?)";
 			$q = $pdo->prepare($sql);
 			$q->execute(array($street1,$street2,$city,$state,$zip,$country));
-			$addressID = $pdo->lastInsertId();
-			$sql = "INSERT INTO customer_address (address_fk, customer_fk) values(?, ?)";
-			$q = $pdo->prepare($sql);
-			$q->execute(array($addressID, $this->customer_id)); 
 			Database::disconnect();
 			return true;
 		}
 	}
-*/
-	public function read(){
+
+	public function read($customer_id){
 		try{
 			$pdo = Database::connect();
 			$sql = 'SELECT * FROM customer WHERE id = ?'
 			$q = $pdo->prepare($sql);
-			$q->execute(array($this->customer_id));
+			$q->execute(array($customer_id));
 			$data = $q->fetchAll(PDO::FETCH_ASSOC);
 	        Database::disconnect();
 	        return $data;
@@ -47,24 +38,24 @@ class customer {
 
     }
 
-	public function update($name, $last_name, $birthdate, $phone_number, $email_address, $user_name, $id){
+	public function update($name, $last_name, $birthdate, $phone_number, $email_address, $user_name, $customer_id){
 		if (!valid($name) || !valid($last_name) || !valid($birthdate) || !valid($phone_number) || !valid($email_address) || !valid($user_name)) {
 			return false;
 		} else {
 			$pdo = Database::connect();
 			$sql = "UPDATE customer SET name = ?, last_name = ?, birthdate = ?, phone_number = ?, email_address = ?, user_name = ? WHERE id = ?";
 			$q = $pdo->prepare($sql);
-			$q->execute(array($name,$last_name,$birthdate,$phone_number,$email_address,$user_name,$id));
+			$q->execute(array($name,$last_name,$birthdate,$phone_number,$email_address,$user_name,$customer_id));
 			Database::disconnect();
 			return true;
 		}
 	}
 
-	public function delete($id){
+	public function delete($customer_id){
         $pdo = Database::connect();
         $sql = "DELETE FROM customer WHERE id = ?";
         $q = $pdo->prepare($sql);
-        $q->execute(array($id));
+        $q->execute(array($customer_id));
         Database::disconnect();
         return true;
 	}
