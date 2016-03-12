0a53e572893d0681d5c341d9441b87717685f7b7
motlj <joshuadamotl@gmail.com>
2016-03-12 17:04:24 -0600

    added die

--- shipmentDelete.php
+++ shipmentDelete.php
@@ -9,10 +9,11 @@
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sql = "DELETE FROM `ecommerce`.`shipment_center` WHERE `id` = ?"; //taken from SQL query on phpMyAdmin
         $q = $pdo->prepare($sql);
-        $q->execute(array($id));
+        $q->execute(query($id));
         Database::disconnect();
         header("Location: adminUpdate.php");
       } catch (PDOException $e) { 
+        die();
         header("Location: adminUpdate.php?error=1");
 /*
       $deleteShipmentCenter = new shipmentCenter($_SESSION['id']);
