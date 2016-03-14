9936dd767875802d3e7937037265449ab9158148
motlj <joshuadamotl@gmail.com>
2016-03-14 10:21:21 -0500

    added div=containers

--- productDetails.php
+++ productDetails.php
@@ -24,63 +24,59 @@ require_once'includes/database.php';
 	     }
       ?>
 
-    <div class="row">
-      <h3>Product Details</h3>
-    </div>
-    
-    <div class="row">
-      <table class="table table-striped table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Action</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-          if($loggedin) {
-              $pdo = Database::connect();
-              $id = $_GET['id'];
-              $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-              $sql = 'SELECT * FROM product WHERE id = ?';
-              $q = $pdo->prepare($sql);
-              $q->execute(array($id));
-              $query = $q->fetchAll(PDO::FETCH_ASSOC);
+    <div class="containter">
+      <div class="row">
+        <h3>Product Details</h3>
+      </div>
+      
+      <div class="row">
+        <table class="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            if($loggedin) {
+                $pdo = Database::connect();
+                $id = $_GET['id'];
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $sql = 'SELECT * FROM product WHERE id = ?';
+                $q = $pdo->prepare($sql);
+                $q->execute(array($id));
+                $query = $q->fetchAll(PDO::FETCH_ASSOC);
 
-            foreach ($query as $row) {
+              foreach ($query as $row) {
 
-                echo '<tr>';
-                echo '<form method="POST" action="cart.php">';
-                echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
-                echo '<td><input type="text" name="product_name" value="'.$row['product_name'].'"></td>'; 
-                echo '<td><input type="text" name="description" value="'.$row['description'].'"></td>'; 
-                echo '<td><input type="text" name="price" value="'.$row['price'].'"></td>';
-                echo '<td><input type="submit" value="Add to Cart"></td>';
-                echo '</form>';
-                echo '</tr>';
-              }
-          }
-          Database::disconnect();
-          ?>
-        </tbody>
-      </table>
-    </div>
+                  echo '<tr>';
+                  echo '<form method="POST" action="cart.php">';
+                  echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
+                  echo '<td><input type="text" name="product_name" value="'.$row['product_name'].'"></td>'; 
+                  echo '<td><input type="text" name="description" value="'.$row['description'].'"></td>'; 
+                  echo '<td><input type="text" name="price" value="'.$row['price'].'"></td>';
+                  echo '<td><input type="submit" value="Add to Cart"></td>';
+                  echo '</form>';
+                  echo '</tr>';
+                }
+            }
+            Database::disconnect();
+            ?>
+          </tbody>
+        </table>
+      </div>
 
-
-
-
-  <div class="container">
-    <div>
-      <a href="index.php">Return to Index</a>
-    </div>
-    <br>
-    <br>
-    <br>
-    <br>
-  </div> <!-- /container -->
+      <div>
+        <a href="index.php">Return to Index</a>
+      </div>
+      <br>
+      <br>
+      <br>
+      <br>
+    </div> <!-- /container -->
 
   <?php 
    require_once('includes/footer.php');
--- productPage.php
+++ productPage.php
@@ -23,64 +23,60 @@ require_once'includes/database.php';
 	       require_once'includes/navbar.php';
 	     }
       ?>
+    <div class="container">
+	    <div class="row">
+	      <h3>List of all Products</h3>
+	    </div>
+	    <div class="row">
+	      <table class="table table-striped table-bordered">
+	        <thead>
+	          <tr>
+	            <th>Name</th>
+	            <th>Description</th>
+	            <th>Price</th>
+	            <th>Action</th>
+	          </tr>
+	        </thead>
+	         <tbody>
+	          <?php
+	          if($loggedin) {
+	          	  $pdo = Database::connect();
+	              $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	              $sql = 'SELECT * FROM product ORDER BY id';
+	              $q = $pdo->prepare($sql);
+	              $q->execute(array());
+	              $query = $q->fetchAll(PDO::FETCH_ASSOC);
 
-    <div class="row">
-      <h3>List of all Products</h3>
-    </div>
-    <div class="row">
-      <table class="table table-striped table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-         <tbody>
-          <?php
-          if($loggedin) {
-          	  $pdo = Database::connect();
-              $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-              $sql = 'SELECT * FROM product ORDER BY id';
-              $q = $pdo->prepare($sql);
-              $q->execute(array());
-              $query = $q->fetchAll(PDO::FETCH_ASSOC);
+	            foreach ($query as $row) {
 
-            foreach ($query as $row) {
+	                echo '<tr>';
+	                echo '<form method="GET" action="productDetails.php">';
+	                echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
+	                echo '<td><input type="text" name="product_name" value="'.$row['product_name'].'"></td>'; 
+	                echo '<td><input type="text" name="price" value="'.$row['price'].'"></td>';
+	                echo '<td><input type="submit" value="More Details"></td>';
+	                echo '</form>';
+	                echo '<form method="POST" action="cart.php">';
+	                echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
+	                echo '<td><input type="submit" value="Add to Cart"></td>';
+	                echo '</form>';
+	                echo '</tr>';
+	            }
+	          } 
+	          Database::disconnect();
+	          ?>
+	         </tbody>
+	      </table>
+	    </div>
 
-                echo '<tr>';
-                echo '<form method="GET" action="productDetails.php">';
-                echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
-                echo '<td><input type="text" name="product_name" value="'.$row['product_name'].'"></td>'; 
-                echo '<td><input type="text" name="price" value="'.$row['price'].'"></td>';
-                echo '<td><input type="submit" value="More Details"></td>';
-                echo '</form>';
-                echo '<form method="POST" action="cart.php">';
-                echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
-                echo '<td><input type="submit" value="Add to Cart"></td>';
-                echo '</form>';
-                echo '</tr>';
-            }
-          } 
-          Database::disconnect();
-          ?>
-        </tbody>
-
-    </div>
-
-
-
-
-  <div class="container">
-    <div>
-      <a href="index.php">Return to Index</a>
-    </div>
-    <br>
-    <br>
-    <br>
-    <br>
-  </div> <!-- /container -->
+        <div>
+          <a href="index.php">Return to Index</a>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+    </div> <!-- /container -->
 
   <?php 
    require_once('includes/footer.php');
