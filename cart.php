2ef0d429cca1a4b4c24ab16e6f113efd8b78d717
motlj <joshuadamotl@gmail.com>
2016-03-14 13:16:27 -0500

    altered foreach loops

--- cart.php
+++ cart.php
@@ -61,12 +61,12 @@ require_once'includes/database.php';
 	                echo '<td><input type="text" name="price" value="'.$row['price'].'"></td>';
 	                echo '<td><input type="text" name="quantity" value="1"></td>';
 	                echo '<td><input type="submit" value="Update Quantity"></td>';
-	                foreach ($query2 as $row2) {
-	                	echo '<form method="POST" action="deleteFromCart.php">';
-		                echo '<input type="hidden" name="id" value="' . $row2['id'] . '">';
-		                echo '<td><input type="submit" value="Remove From Cart"></td>';
-		                echo '</form>';
-	                }
+	            }
+	            foreach ($query2 as $row2) {
+	               	echo '<form method="POST" action="deleteFromCart.php">';
+		            echo '<input type="hidden" name="id" value="' . $row2['id'] . '">';
+		            echo '<td><input type="submit" value="Remove From Cart"></td>';
+		            echo '</form>';
 	                echo '</tr>';
 	            }
 	          } 
