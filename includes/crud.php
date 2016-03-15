0de839996cd2a4433babf3677cf680f68f0c4165
motlj <joshuadamotl@gmail.com>
2016-03-15 18:19:00 -0500

    removed cart create from login, added session to crud

--- includes/crud.php
+++ includes/crud.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'includes/session.php';
 
 // helper function for validation
 function valid($varname){
--- includes/login.php
+++ includes/login.php
@@ -15,9 +15,6 @@ require_once('database.php');
 		    $q = $pdo->prepare($sql);
        		$q->execute(array($username,$loginpassword));
        		$query = $q->fetch(PDO::FETCH_ASSOC);
-       		$sql2 = 'INSERT INTO transaction (customer_fk) values (?)';
-       		$q2 = $pdo->prepare($sql2);
-       		$q2->execute(array($query['id']));
 		    Database::disconnect();
 
        		$name = $query['name'];
