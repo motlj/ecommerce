59c9b1522a830de3c9bb146a754b1a1fa3fb2cd5
motlj <joshuadamotl@gmail.com>
2016-03-15 20:38:20 -0500

    fixed table

--- categories.php
+++ categories.php
@@ -17,38 +17,61 @@ require_once 'includes/database.php';
 
   <body>
     <div class="container">
+      <div class="row">
+        <h3>List of all Products</h3>
+      </div>
+      <div class="row">
+        <table class="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Action</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+           <tbody>
     
-      <?php 
-      if ($admin) {
-        require_once'includes/adminNavBar.php';
-      } else {
-        require_once'includes/navbar.php';
-      }
+              <?php 
+              if ($admin) {
+                require_once'includes/adminNavBar.php';
+              } else {
+                require_once'includes/navbar.php';
+              }
 
-      $category_id = $_GET['id'];
-      $pdo = Database::connect();
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $sql = 'SELECT * FROM product WHERE category_fk = ?';
-      $q = $pdo->prepare($sql);
-      $q->execute(array($category_id));
-      $products = $q->fetchAll();
+              $category_id = $_GET['id'];
+              $pdo = Database::connect();
+              $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+              $sql = 'SELECT * FROM product WHERE category_fk = ?';
+              $q = $pdo->prepare($sql);
+              $q->execute(array($category_id));
+              $products = $q->fetchAll();
 
-      foreach ($products as $row) {
-        echo '<tr>';
-        echo '<form method="GET" action="productDetails.php">';
-        echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
-        echo '<td><input type="text" name="product_name" value="'.$row['product_name'].'"></td>'; 
-        echo '<td><input type="text" name="price" value="'.$row['price'].'"></td>';
-        echo '<td><input type="submit" value="More Details"></td>';
-        echo '</form>';
-        echo '<form method="POST" action="addToCart.php">';
-        echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
-        echo '<td><input type="submit" value="Add to Cart"></td>';
-        echo '</form>';
-        echo '</tr>';
-      }
-      ?>
-
+              foreach ($products as $row) {
+                echo '<tr>';
+                echo '<form method="GET" action="productDetails.php">';
+                echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
+                echo '<td><input type="text" name="product_name" value="'.$row['product_name'].'"></td>'; 
+                echo '<td><input type="text" name="price" value="'.$row['price'].'"></td>';
+                echo '<td><input type="submit" value="More Details"></td>';
+                echo '</form>';
+                echo '<form method="POST" action="addToCart.php">';
+                echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
+                echo '<td><input type="submit" value="Add to Cart"></td>';
+                echo '</form>';
+                echo '</tr>';
+              }
+              ?>
+           </tbody>
+        </table>
+      </div>
+      <div>
+        <a href="index.php">Return to Index</a>
+      </div>
+      <br>
+      <br>
+      <br>
+      <br>
     </div><!-- /.container -->
 
     <?php require_once('includes/footer.php');?>
