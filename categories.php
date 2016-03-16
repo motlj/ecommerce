5b44df5d2eb69afc4459296eb8452ab764ef5569
motlj <joshuadamotl@gmail.com>
2016-03-15 20:35:27 -0500

    added table

--- categories.php
+++ categories.php
@@ -16,36 +16,44 @@ require_once 'includes/database.php';
  </head>
 
   <body>
-    <?php 
-    if ($admin) {
-      require_once'includes/adminNavBar.php';
-    } else {
-      require_once'includes/navbar.php';
-    }
-    echo $_GET['id'];
-
-    $category_id = $_GET['id'];
-    $pdo = Database::connect();
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = 'SELECT * FROM product WHERE category_fk = ?';
-    $q = $pdo->prepare($sql);
-    $q->execute(array($category_id));
-    $products = $q->fetchAll();
-
-    print_r($products);
-
-
-
-    ?>
-
     <div class="container">
+    
+      <?php 
+      if ($admin) {
+        require_once'includes/adminNavBar.php';
+      } else {
+        require_once'includes/navbar.php';
+      }
 
+      $category_id = $_GET['id'];
+      $pdo = Database::connect();
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = 'SELECT * FROM product WHERE category_fk = ?';
+      $q = $pdo->prepare($sql);
+      $q->execute(array($category_id));
+      $products = $q->fetchAll();
+
+      foreach ($products as $row) {
+        echo '<tr>';
+        echo '<form method="GET" action="productDetails.php">';
+        echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
+        echo '<td><input type="text" name="product_name" value="'.$row['product_name'].'"></td>'; 
+        echo '<td><input type="text" name="price" value="'.$row['price'].'"></td>';
+        echo '<td><input type="submit" value="More Details"></td>';
+        echo '</form>';
+        echo '<form method="POST" action="addToCart.php">';
+        echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
+        echo '<td><input type="submit" value="Add to Cart"></td>';
+        echo '</form>';
+        echo '</tr>';
+      }
+      ?>
 
     </div><!-- /.container -->
 
     <?php require_once('includes/footer.php');?>
 
-  <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
+   <script src="assets/js/jquery.min.js"></script>
+   <script src="assets/js/bootstrap.min.js"></script>
   </body>
 </html>
