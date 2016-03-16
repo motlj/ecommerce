c9aedb0e8f64d60f38db9f138b09a3cf85c66923
motlj <joshuadamotl@gmail.com>
2016-03-15 21:50:16 -0500

    commented out most of arraypush

--- includes/crud.php
+++ includes/crud.php
@@ -306,7 +306,7 @@ class cart {
 			$q = $pdo->prepare($sql);
 			$q->execute(array($item['product_fk']));
 			$product = $q->fetch(PDO::FETCH_ASSOC);
-			//array_push($products, array("id"=>$item['id'], "product_fk" $item['product_fk'], "quantity"=>$item['quantity'], "product_name"=>$product['product_name'], "price"=>$product['price'], "description"=>$product['description']));
+			array_push($products, array("id"=>$item['id']/*, "product_fk" $item['product_fk'], "quantity"=>$item['quantity'], "product_name"=>$product['product_name'], "price"=>$product['price'], "description"=>$product['description']*/));
 		}
 		Database::disconnect();
 		return $products;
