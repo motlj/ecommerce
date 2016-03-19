04b01d155a2e28ae1287bb36547373f509b0defb
motlj <joshuadamotl@gmail.com>
2016-03-19 00:57:19 -0500

    returned to jpg image

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ require_once('includes/database.php');
         <tbody>
           <tr id="tablebody">
             <td>
-              <img src="assets/img/woodwisconsinlow.png">
+              <img src="assets/img/redhome/red1.jpg">
             </td>
             <td>
               <h2>$100.00</h2>
