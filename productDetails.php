137eb8da148cf6884e2f8db9f272e89db07b58da
motlj <joshuadamotl@gmail.com>
2016-03-19 02:24:43 -0500

    formatted productDetails to better display product

--- productDetails.php
+++ productDetails.php
@@ -29,80 +29,87 @@ require_once'includes/database.php';
 
     <div class="container">
       <div class="row">
-        <?php
-          $id = $_GET['id'];
-          $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          $sql = 'SELECT product_name FROM product WHERE id = ?';
-          $q = $pdo->prepare($sql);
-          $q->execute(array($id));
-          $query = $q->fetch(PDO::FETCH_ASSOC);
+        <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12"> 
+          <?php
+            $id = $_GET['id'];
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = 'SELECT product_name FROM product WHERE id = ?';
+            $q = $pdo->prepare($sql);
+            $q->execute(array($id));
+            $query = $q->fetch(PDO::FETCH_ASSOC);
 
-          echo '<h3>' . $query['product_name'] . '</h3>';
-        ?>
-      </div>
-      
-      <?php
-        $id = $_GET['id'];
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = 'SELECT * FROM image WHERE product_fk = ? AND featured = 1';
-        $q = $pdo->prepare($sql);
-        $q->execute(array($id));
-        $query = $q->fetchAll(PDO::FETCH_ASSOC);
+            echo '<h3>' . $query['product_name'] . '</h3>';
+          ?>
+          
+          <hr>
+        
+          <?php
+            $id = $_GET['id'];
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = 'SELECT * FROM image WHERE product_fk = ? AND featured = 1';
+            $q = $pdo->prepare($sql);
+            $q->execute(array($id));
+            $query = $q->fetchAll(PDO::FETCH_ASSOC);
 
-        foreach ($query as $image) {
-          echo '<img id="productImageSize" src="' . $image['image_link'] . '">';
-        }
-
-        $sql2 = 'SELECT * FROM image WHERE product_fk = ? AND featured = 0';
-        $q2 = $pdo->prepare($sql2);
-        $q2->execute(array($id));
-        $query2 = $q2->fetchAll(PDO::FETCH_ASSOC);
-
-        foreach ($query2 as $image2) {
-          echo '<img class="img-thumbnail thumbnail" src="' . $image2['image_link'] . '">';
-        }
-
-      ?>
-
-
-      <div class="row">
-        <table class="table table-striped table-bordered">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Price</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
-            if($loggedin) {
-                $pdo = Database::connect();
-                $id = $_GET['id'];
-                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $sql = 'SELECT * FROM product WHERE id = ?';
-                $q = $pdo->prepare($sql);
-                $q->execute(array($id));
-                $query = $q->fetchAll(PDO::FETCH_ASSOC);
-
-              foreach ($query as $row) {
-
-                  echo '<tr>';
-                  echo '<form method="POST" action="addToCart.php">';
-                  echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
-                  echo '<td>'.$row['product_name'].'</td>'; 
-                  echo '<td>'.$row['description'].'</td>'; 
-                  echo '<td>'.$row['price'].'</td>';
-                  echo '<td><input type="submit" value="Add to Cart"></td>';
-                  echo '</form>';
-                  echo '</tr>';
-                }
+            foreach ($query as $image) {
+              echo '<img id="productImageSize" src="' . $image['image_link'] . '"><br>';
             }
-            Database::disconnect();
-            ?>
-          </tbody>
-        </table>
+
+            $sql2 = 'SELECT * FROM image WHERE product_fk = ? AND featured = 0';
+            $q2 = $pdo->prepare($sql2);
+            $q2->execute(array($id));
+            $query2 = $q2->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach ($query2 as $image2) {
+              echo '<img class="img-thumbnail thumbnail" src="' . $image2['image_link'] . '">';
+            }
+          ?>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+          <table class="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th colspan="2">Product Information</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+              if($loggedin) {
+                  $pdo = Database::connect();
+                  $id = $_GET['id'];
+                  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                  $sql = 'SELECT * FROM product WHERE id = ?';
+                  $q = $pdo->prepare($sql);
+                  $q->execute(array($id));
+                  $query = $q->fetchAll(PDO::FETCH_ASSOC);
+
+                foreach ($query as $row) {
+
+                    echo '<tr>';
+                    echo '<form method="POST" action="addToCart.php">';
+                    echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
+                    echo '<td>Product:</td>';
+                    echo '<td>'.$row['product_name'].'</td>';
+                    echo '</tr>';
+                    echo '<tr>';
+                    echo '<td>Description:</td>';
+                    echo '<td>'.$row['description'].'</td>'; 
+                    echo '</tr>';
+                    echo '<tr>';
+                    echo '<td>Description:</td>';
+                    echo '<td>'.$row['price'].'</td>';
+                    echo '</tr>';
+                    echo '<tr>';
+                    echo '<td colspan="2"><input type="submit" value="Add to Cart"></td>';
+                    echo '</form>';
+                    echo '</tr>';
+                  }
+              }
+              Database::disconnect();
+              ?>
+            </tbody>
+          </table>
       </div>
 
       <div>
