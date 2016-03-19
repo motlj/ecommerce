2cd57e87ba4c4179aa9b99f8500c210e25f1b9b5
motlj <joshuadamotl@gmail.com>
2016-03-19 11:43:11 -0500

    changed fomrat

--- search.php
+++ search.php
@@ -46,13 +46,13 @@ require_once 'includes/database.php';
     
             <?php
               $search = $_POST['search'];
-              $sqlSearch = '%' . $search . '%';
+              //$sqlSearch = '%' . $search . '%';
             try {
               $pdo = Database::connect();
               $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
               $sql = "SELECT * FROM product WHERE product_name LIKE :search";
               $q = $pdo->prepare($sql);
-              $q->bindValue(':search', $sqlSearch, PDO::PARAM_INT);
+              $q->bindValue(':search', '%' . $search . '%', PDO::PARAM_INT);
               $q->execute();
               $products = $q->fetchAll(FETCH_ASSOC);
               print_r($products);
