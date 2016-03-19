1d9d4dca1d4efbfc69286891bc112e8987ba1cf0
motlj <joshuadamotl@gmail.com>
2016-03-19 04:58:45 -0500

    resized images

--- index.php
+++ index.php
@@ -43,19 +43,19 @@ require_once('includes/database.php');
       <hr>
       <div class="row">
         <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-          <a href="categories.php?id=12"><img class="imagecenter" alt="Bottle Opener" title="Bottle Opener" src="assets/img/bottleopener.png"></a>
+          <a href="categories.php?id=12"><img class="imagewidth" alt="Bottle Opener" title="Bottle Opener" src="assets/img/bottleopener.png"></a>
           <br>
           <a href="categories.php?id=12"><h5 class="center">Bottle Openers</h5></a>
           <br>
         </div>
         <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-          <a href="categories.php?id=14"><img class="imagecenter" alt="Pipe Decorations" title="Pipe Decorations" src="assets/img/pipelamp.png"></a>
+          <a href="categories.php?id=14"><img class="imagewidth" alt="Pipe Decorations" title="Pipe Decorations" src="assets/img/pipelamp.png"></a>
           <br>
           <a href="categories.php?id=14"><h5 class="center">Decorative Pipe Creations</h5></a>
           <br>
         </div>
         <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-          <a href="categories.php?id=11"><img class="imagecenter" alt="Wooden Signs Wisconsin Home" title="Wooden Signs Wisconsin Home" src="assets/img/home.png"></a>
+          <a href="categories.php?id=11"><img class="imagewidth" alt="Wooden Signs Wisconsin Home" title="Wooden Signs Wisconsin Home" src="assets/img/home.png"></a>
           <br>
           <a href="categories.php?id=11"><h5 class="center">Wooden Signs</h5></a>
           <br>
