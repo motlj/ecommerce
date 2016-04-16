b816e5b086a9ada7cfadb448f03def0669bdd5de
motlj <joshuadamotl@gmail.com>
2016-04-16 13:42:50 -0500

    syntax error

--- searchFunction.js
+++ searchFunction.js
@@ -15,10 +15,10 @@ $(document).ready(function(){
 					if (results) {
 						$.each($.parseJSON(results), function(key, value){
 							$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><h1>' + value.product_name + '</h1><p>' + value['description'] + '</p><h3>' + value.price + '</h3><form method="GET" action="productDetails.php"><input type="hidden" name="id" value="' + value['id'] + '"><input type="submit" value="More Details"></form><hr></div>');
-						} else {
-							$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><p>Your search did not return any results. Please try again.</p></div>');
-						}
-					});
+						});						
+					} else {
+						$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><p>Your search did not return any results. Please try again.</p></div>');
+					}
 				}
 			});
 		}
