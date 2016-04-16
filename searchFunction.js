18b8eac55df62947405d2e7d1c9a1e1bb9dce50b
motlj <joshuadamotl@gmail.com>
2016-04-16 13:18:26 -0500

    added button for more details to search results

--- searchFunction.js
+++ searchFunction.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 				
 			$('#searchResults').html("");
 				$.each($.parseJSON(results), function(key, value){
-					$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><h1>' + value.product_name + '</h1><p>' + value['description'] + '</p><h3>' + value.price + '</h3></div>');
+					$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><h1>' + value.product_name + '</h1><p>' + value['description'] + '</p><h3>' + value.price + '</h3><form method="GET" action="productDetails.php"><input type="hidden" name="id" value="' + value['id'] + '"></form></div>');
 				});
 
 				//return results from json file using php
