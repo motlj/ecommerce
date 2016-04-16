239d1a89569aa2792e69abc9c1cd81c966879482
motlj <joshuadamotl@gmail.com>
2016-04-16 13:28:25 -0500

    put search into if statement

--- searchFunction.js
+++ searchFunction.js
@@ -1,25 +1,21 @@
 $(document).ready(function(){
-	$("#srch-term").on('input', function(){
-		return $.ajax({
-			type: "POST",
-			datatype : "json",
-			url: "liveSearch.php",
-			data: { terms: $("#srch-term").val() },
-			success : function(results){
-				//console.log(results);
-				
-			$('#searchResults').html("");
-				$.each($.parseJSON(results), function(key, value){
-					$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><h1>' + value.product_name + '</h1><p>' + value['description'] + '</p><h3>' + value.price + '</h3><form method="GET" action="productDetails.php"><input type="hidden" name="id" value="' + value['id'] + '"><input type="submit" value="More Details"></form><hr></div>');
-				});
-
-				//return results from json file using php
-				//using post send input to search.php
-				//search.php will take the input and use pdo to fetchAll
-				//convert php array to json
-				//set fetchAll to return an array in a json object
-				//jquery pagination
-			}
+	$("#srch-term").on('input', function(){			
+		$('#searchResults').html("");
+		var searchTerm = $("#searchResults").val();
+		if (searchTerm != null && searchTerm != "") {
+			return $.ajax({
+				type: "POST",
+				datatype : "json",
+				url: "liveSearch.php",
+				data: { terms: searchTerm },
+				success : function(results){
+					//console.log(results);
+					
+					$.each($.parseJSON(results), function(key, value){
+						$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><h1>' + value.product_name + '</h1><p>' + value['description'] + '</p><h3>' + value.price + '</h3><form method="GET" action="productDetails.php"><input type="hidden" name="id" value="' + value['id'] + '"><input type="submit" value="More Details"></form><hr></div>');
+					});
+				}
+			})
 		})
-	})
+	};
 })
