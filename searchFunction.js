78cc78912b3299610dd3861155f3cae79814a6e3
motlj <joshuadamotl@gmail.com>
2016-04-16 14:12:17 -0500

    removed comments

--- searchFunction.js
+++ searchFunction.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
 							$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><h1>' + value.product_name + '</h1><p>' + value['description'] + '</p><h3>' + value.price + '</h3><form method="GET" action="productDetails.php"><input type="hidden" name="id" value="' + value['id'] + '"><input type="submit" value="More Details"></form><hr></div>');
 						});						
 					} else {
-						//$('#searchResults').append('<div class="col-xs-12 //col-md-12 col-lg-12"><p>Your search did not return //any results. Please try again.</p></div>');
+						$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><p>Your search did not return any results. Please try again.</p></div>');
 					}
 				}
 			});
