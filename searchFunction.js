f72a9ab0c4163fc130b4557c4f7221d657684096
motlj <joshuadamotl@gmail.com>
2016-04-16 12:15:32 -0500

    removed lines and changed description call

--- searchFunction.js
+++ searchFunction.js
@@ -7,12 +7,9 @@ $(document).ready(function(){
 			data: { terms: $("#srch-term").val() },
 			success : function(results){
 				console.log(results);
-				$('#searchResults').append('<div class="row">');
-				$.each(results.items, function(key, value){
-					$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><h1>' + value.product_name + '</h1><p>' + value.description + '</p><h2>' + value.price + '</h2></div>');
-				
+				$.each(results, function(key, value){
+					$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><h1>' + value.product_name + '</h1><p>' + value['description'] + '</p><h2>' + value.price + '</h2></div>');
 				});
-				$('#searchResults').append('</div>');
 				//return results from json file using php
 				//using post send input to search.php
 				//search.php will take the input and use pdo to fetchAll
