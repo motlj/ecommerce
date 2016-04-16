a24cc32261ef8c89a0c7ea37ef07e16cb54d07b7
motlj <joshuadamotl@gmail.com>
2016-04-16 14:01:10 -0500

    console.log(items)

--- searchFunction.js
+++ searchFunction.js
@@ -12,13 +12,15 @@ $(document).ready(function(){
 				data: { terms: searchTerm },
 				success : function(results){
 					//console.log(results);
-					if (results != null) {
-						$.each($.parseJSON(results), function(key, value){
+					var items = $.parseJSON(results);
+					console.log(items);
+					//if () {
+						$.each(items, function(key, value){
 							$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><h1>' + value.product_name + '</h1><p>' + value['description'] + '</p><h3>' + value.price + '</h3><form method="GET" action="productDetails.php"><input type="hidden" name="id" value="' + value['id'] + '"><input type="submit" value="More Details"></form><hr></div>');
 						});						
-					} else {
-						$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><p>Your search did not return any results. Please try again.</p></div>');
-					}
+					//} else {
+						//$('#searchResults').append('<div class="col-xs-12 //col-md-12 col-lg-12"><p>Your search did not return //any results. Please try again.</p></div>');
+					//}
 				}
 			});
 		}
