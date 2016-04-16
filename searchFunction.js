396e8870fd353903fccc91e64c9ef7a47b0bf454
motlj <joshuadamotl@gmail.com>
2016-04-16 13:41:32 -0500

    put if statement for no results found

--- searchFunction.js
+++ searchFunction.js
@@ -12,9 +12,12 @@ $(document).ready(function(){
 				data: { terms: searchTerm },
 				success : function(results){
 					//console.log(results);
-					
-					$.each($.parseJSON(results), function(key, value){
-						$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><h1>' + value.product_name + '</h1><p>' + value['description'] + '</p><h3>' + value.price + '</h3><form method="GET" action="productDetails.php"><input type="hidden" name="id" value="' + value['id'] + '"><input type="submit" value="More Details"></form><hr></div>');
+					if (results) {
+						$.each($.parseJSON(results), function(key, value){
+							$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><h1>' + value.product_name + '</h1><p>' + value['description'] + '</p><h3>' + value.price + '</h3><form method="GET" action="productDetails.php"><input type="hidden" name="id" value="' + value['id'] + '"><input type="submit" value="More Details"></form><hr></div>');
+						} else {
+							$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><p>Your search did not return any results. Please try again.</p></div>');
+						}
 					});
 				}
 			});
