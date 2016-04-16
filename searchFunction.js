0c97a64bce3de5351c773c42be7ae9a7735a1289
motlj <joshuadamotl@gmail.com>
2016-04-16 14:07:46 -0500

    attempting to add error message for no results

--- searchFunction.js
+++ searchFunction.js
@@ -14,13 +14,13 @@ $(document).ready(function(){
 					//console.log(results);
 					var items = $.parseJSON(results);
 					console.log(items);
-					//if () {
+					if (index < items.length) {
 						$.each(items, function(key, value){
 							$('#searchResults').append('<div class="col-xs-12 col-md-12 col-lg-12"><h1>' + value.product_name + '</h1><p>' + value['description'] + '</p><h3>' + value.price + '</h3><form method="GET" action="productDetails.php"><input type="hidden" name="id" value="' + value['id'] + '"><input type="submit" value="More Details"></form><hr></div>');
 						});						
-					//} else {
+					} else {
 						//$('#searchResults').append('<div class="col-xs-12 //col-md-12 col-lg-12"><p>Your search did not return //any results. Please try again.</p></div>');
-					//}
+					}
 				}
 			});
 		}
