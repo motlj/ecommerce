616bcd3cfea65e16e5bb0e1f56b8cd724492b844
motlj <joshuadamotl@gmail.com>
2016-04-16 10:22:52 -0500

    fixed syntax error

--- searchFunction.js
+++ searchFunction.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 		$.ajax({
 			method: "POST",
 			datatype : "json",
-			url: 'liveSearch.php?srch-term=' + this.val,
+			url: 'liveSearch.php?srch-term=' + this.val(),
 			data: { terms: $("#srch-term").val() },
 			success : function(results){
 				console.log(results);
