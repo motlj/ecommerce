0af70c1e51cdb2b3c00bfa68cd904b530e9ce60b
motlj <joshuadamotl@gmail.com>
2016-05-16 21:21:31 -0500

    added search to both pages

--- loginpage.php
+++ loginpage.php
@@ -13,26 +13,30 @@
   <body>
     <?php require_once('includes/navbar.php');?>
 
-    
-    <div class="container">
-      <div class="starter-template">
-        <h1>Login</h1>
-        <p class="lead">Please enter username and password to login to your account.<br></p>
-      </div>
-      <div>
-        <form action="includes/login.php" method="post">
-          <input type="text" name="user_name" placeholder="user_name">
-          <input type="password" name="password" placeholder="password">
-          <input type="submit" value="Login">        
-        </form>
-      </div>
-      <div>
-      	<p>If you do not have an account please click register below</p>
-      </div>
-      <div>
-        <form action="register.php" method="post">
-          <input type="submit" value="Register">
-        </form>
+    <div id="searchResults" class="row">
+    </div>
+
+    <div id="hidden">
+      <div class="container">
+        <div class="starter-template">
+          <h1>Login</h1>
+          <p class="lead">Please enter username and password to login to your account.<br></p>
+        </div>
+        <div>
+          <form action="includes/login.php" method="post">
+            <input type="text" name="user_name" placeholder="user_name">
+            <input type="password" name="password" placeholder="password">
+            <input type="submit" value="Login">        
+          </form>
+        </div>
+        <div>
+        	<p>If you do not have an account please click register below</p>
+        </div>
+        <div>
+          <form action="register.php" method="post">
+            <input type="submit" value="Register">
+          </form>
+        </div>
       </div>
     </div>
 
