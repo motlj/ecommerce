d2ab8b28badaae9a6183728a026db94ce7a2c967
motlj <joshuadamotl@gmail.com>
2016-05-16 21:27:20 -0500

    removed redundant titles

--- register.php
+++ register.php
@@ -61,9 +61,9 @@
             <h3>Please fill out all fields to register.</h3>
           </div>           
           <form action="register.php" method="post"> 
-
+            <br>
             <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
-              <label class="control-label">Name</label>
+              <!-- <label class="control-label">Name</label> -->
               <div class="controls">
                 <input name="name" type="text" placeholder="Name" value="<?php echo !empty($name)?$name:'';?>">
                 <?php if (!empty($nameError)): ?>
@@ -71,9 +71,9 @@
                 <?php endif;?>
               </div>
             </div>
-
+            <br>
             <div class="control-group <?php echo !empty($last_nameError)?'error':'';?>">
-              <label class="control-label">Last Name</label>
+              <!-- <label class="control-label">Last Name</label> -->
               <div class="controls">
                 <input name="last_name" type="text" placeholder="Last Name" value="<?php echo !empty($last_name)?$last_name:'';?>">
                 <?php if (!empty($last_nameError)): ?>
@@ -81,9 +81,9 @@
                 <?php endif;?>
               </div>
             </div>
-
+            <br>
             <div class="control-group <?php echo !empty($birthdateError)?'error':'';?>">
-              <label class="control-label">Birthdate</label>
+              <!-- <label class="control-label">Birthdate</label> -->
               <div class="controls">
                 <input name="birthdate" type="text" placeholder="Birthdate" value="<?php echo !empty($birthdate)?$birthdate:'';?>">
                 <?php if (!empty($birthdateError)): ?>
@@ -91,9 +91,9 @@
                 <?php endif;?>
               </div>
             </div>
-
+            <br>
             <div class="control-group <?php echo !empty($phone_numberError)?'error':'';?>">
-              <label class="control-label">Phone Number</label>
+              <!-- <label class="control-label">Phone Number</label> -->
               <div class="controls">
                 <input name="phone_number" type="text" placeholder="Phone Number" value="<?php echo !empty($phone_number)?$phone_number:'';?>">
                 <?php if (!empty($phone_numberError)): ?>
@@ -101,9 +101,9 @@
                 <?php endif;?>
               </div>
             </div>
-
+            <br>
             <div class="control-group <?php echo !empty($email_addressError)?'error':'';?>">
-              <label class="control-label">Email Address</label>
+              <!-- <label class="control-label">Email Address</label> -->
               <div class="controls">
                 <input name="email_address" type="text" placeholder="Email Address" value="<?php echo !empty($email_address)?$email_address:'';?>">
                 <?php if (!empty($email_addressError)): ?>
@@ -111,9 +111,9 @@
                 <?php endif;?>
               </div>
             </div>
-
+            <br>
             <div class="control-group <?php echo !empty($user_nameError)?'error':'';?>">
-              <label class="control-label">User Name</label>
+              <!-- <label class="control-label">User Name</label> -->
               <div class="controls">
                 <input name="user_name" type="text" placeholder="User Name" value="<?php echo !empty($user_name)?$user_name:'';?>">
                 <?php if (!empty($user_nameError)): ?>
@@ -121,9 +121,9 @@
                 <?php endif;?>
               </div>
             </div>
-
+            <br>
             <div class="control-group <?php echo !empty($passwordError)?'error':'';?>">
-              <label class="control-label">Password</label>
+              <!-- <label class="control-label">Password</label> -->
               <div class="controls">
                 <input name="password" type="text" placeholder="Password" value="<?php echo !empty($password)?$password:'';?>">
                 <?php if (!empty($passwordError)): ?>
