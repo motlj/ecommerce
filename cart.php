9f215e6946c54ff43e05edd37c3c5b75b4883016
motlj <joshuadamotl@gmail.com>
2016-06-28 23:52:01 -0500

    modified search function
    added search to checkout and hid all content in cart when search
    function runs

--- cart.php
+++ cart.php
@@ -26,85 +26,87 @@ require_once'includes/crud.php';
     	
     	<div id="searchResults" class="row">
         </div>
-	    
-	    <div class="row">
-	      <br><br>
-	      <center><h1>Your Shopping Cart</h1></center>
-	      <br><br><br>
-	    </div>
-	    <?php
-	    if($loggedin) {
-	    	$fetchCart = new cart();				
-			$products = $fetchCart->fetchCart();
-			$cost = 0;
-			echo '<div class="row">';
-            echo '<form method="POST" action="updateQuantity.php">';
-            echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
 
-			foreach ($products as $row) {
-
-            	//echo '<div class="row">this is the product id:' . $row['product_fk'] . '</p></div>';
-
-                $pdo = Database::connect();
-                $sql2 = 'SELECT image_link FROM image WHERE product_fk = ? AND featured = 1';
-                $q2 = $pdo->prepare($sql2);
-                $q2->execute(array($row['product_fk']));
-                $thumbnail = $q2->fetch();
-
-                //echo '<div class="row"><p>' . $thumbnail['image_link'] . '</p></div>';
-                echo '<div class="col-lg-3 col-md-3 col-sm-12"><center><img id="cartImage" src="'. $thumbnail['image_link'] . '"></center></div>';
-
-                echo '<div class="col-lg-1 col-md-1 col-sm-0"></div>';
-          	    echo '<div class="col-lg-8 col-md-8 col-sm-12">';
-
-          	    echo '<h2>' . $row['product_name'] . '  -  <em>$' . $row['price'] . '</em></h2>';
-                echo '<input type="text" size="2" name="quantity" value="' . $row['quantity'] . '">&nbsp;&nbsp;';
-                echo '<input type="submit" class="btn btn-success form-actions" value="Update Quantity">';
-                $cost = $cost + (($row['price']) * ($row['quantity']));
-                echo '</form><br>';
-                echo '<form method="POST" action="deleteFromCart.php">';
+	    <div id="hidden">
+		    <div class="row">
+		      <br><br>
+		      <center><h1>Your Shopping Cart</h1></center>
+		      <br><hr><br>
+		    </div>
+		    <?php
+		    if($loggedin) {
+		    	$fetchCart = new cart();				
+				$products = $fetchCart->fetchCart();
+				$cost = 0;
+				echo '<div class="row">';
+	            echo '<form method="POST" action="updateQuantity.php">';
 	            echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
-	            //echo '<p>this is the id:' . $row['id'] . '</p>';
-	            echo '<br><input type="submit" class="btn btn-danger form-actions" value="Remove From Cart">';
-	            echo '</form>';
-	            echo '</div>';
-	            echo '</div>';
-	            echo '<hr>';
-    		}
-    		echo '<br>';
-    		echo '<center><div class="row">';
-            echo '<h4>Subtotal:  $' . $cost . '</h4>';
-            echo '</div>';
-            $tax = ($cost * .056);   
-            $roundedTax = number_format((float)$tax,2, '.', '');         
-            echo '<div class="row">';
-            echo '<h4>Tax:  $' . $roundedTax . '</h4>';
-            echo '</div>';
-            $total = $roundedTax + $cost;
-            $roundedTotal = number_format((float)$total,2, '.', '');
-            echo '<div class="row">';
-            echo '<h3>Total:  $' . $roundedTotal . '</h3>';
-            echo '</div>';
-    	}
-    	Database::disconnect();
-	    ?>
 
-        <div><center>
-        	<form method="POST" action="checkout.php">
-        		<input type="submit" class="btn btn-success form-actions" value="Checkout">
-        	</form>
-          <!-- <a href="checkout.php">Checkout</a> -->
-        <center></div>
-        <br>
-        <br>
+				foreach ($products as $row) {
 
-        <div><center>
-          <a href="index.php">Return to Index</a>
-        </div>
-        <br>
-        <br>
-        <br>
-        <br>
+	            	//echo '<div class="row">this is the product id:' . $row['product_fk'] . '</p></div>';
+
+	                $pdo = Database::connect();
+	                $sql2 = 'SELECT image_link FROM image WHERE product_fk = ? AND featured = 1';
+	                $q2 = $pdo->prepare($sql2);
+	                $q2->execute(array($row['product_fk']));
+	                $thumbnail = $q2->fetch();
+
+	                //echo '<div class="row"><p>' . $thumbnail['image_link'] . '</p></div>';
+	                echo '<div class="col-lg-3 col-md-3 col-sm-12"><center><img id="cartImage" src="'. $thumbnail['image_link'] . '"></center></div>';
+
+	                echo '<div class="col-lg-1 col-md-1 col-sm-0"></div>';
+	          	    echo '<div class="col-lg-8 col-md-8 col-sm-12">';
+
+	          	    echo '<h2>' . $row['product_name'] . '  -  <em>$' . $row['price'] . '</em></h2>';
+	                echo '<input type="text" size="2" name="quantity" value="' . $row['quantity'] . '">&nbsp;&nbsp;';
+	                echo '<input type="submit" class="btn btn-success form-actions" value="Update Quantity">';
+	                $cost = $cost + (($row['price']) * ($row['quantity']));
+	                echo '</form><br>';
+	                echo '<form method="POST" action="deleteFromCart.php">';
+		            echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
+		            //echo '<p>this is the id:' . $row['id'] . '</p>';
+		            echo '<br><input type="submit" class="btn btn-danger form-actions" value="Remove From Cart">';
+		            echo '</form>';
+		            echo '</div>';
+		            echo '</div>';
+		            echo '<hr>';
+	    		}
+	    		echo '<br>';
+	    		echo '<center><div class="row">';
+	            echo '<h4>Subtotal:  $' . $cost . '</h4>';
+	            echo '</div>';
+	            $tax = ($cost * .056);   
+	            $roundedTax = number_format((float)$tax,2, '.', '');         
+	            echo '<div class="row">';
+	            echo '<h4>Tax:  $' . $roundedTax . '</h4>';
+	            echo '</div>';
+	            $total = $roundedTax + $cost;
+	            $roundedTotal = number_format((float)$total,2, '.', '');
+	            echo '<div class="row">';
+	            echo '<h3>Total:  $' . $roundedTotal . '</h3>';
+	            echo '</div>';
+	    	}
+	    	Database::disconnect();
+		    ?>
+
+	        <div><center>
+	        	<form method="POST" action="checkout.php">
+	        		<input type="submit" class="btn btn-success form-actions" value="Checkout">
+	        	</form>
+	          <!-- <a href="checkout.php">Checkout</a> -->
+	        <center></div>
+	        <br>
+	        <br>
+
+	        <div><center>
+	          <a href="index.php">Return to Index</a>
+	        </div>
+	        <br>
+	        <br>
+	        <br>
+	        <br>
+	    </div>
     </center></div> <!-- /container -->
 
   <?php 
--- checkout.php
+++ checkout.php
@@ -25,77 +25,83 @@ require_once'includes/crud.php';
 	     }
       ?>
     <div class="container">
-	    <div class="row">
-	        <h3>Verify Shipping Information</h3>
-	        <h4>Personal Information:</h4>
-				<?php
-					$customer = new customer();
-					$cust = $customer->read($_SESSION['id']);
-				    
-				    echo '<form method="POST" action="placeOrderFunction.php">';
-				    echo '<input type="hidden" name="id" value="' . $cust['id'] . '">';
-				    echo ''.$cust['name'].'';
-				    echo '&nbsp;'; 
-				    echo ''.$cust['last_name'].'';
-				    echo '<br>';
-				    echo ''.$cust['phone_number'].'';
-				    echo '<br>';
-				    echo ''.$cust['email_address'].'';
-				?>
-			<br>
-			<br>
-			<br>
-	      	<h4>Select Shipping Address</h4>
-	      	<p>If you need to add a new address, please do so <a href="addressCreate.php">here</a>.</p>
-		    	<?php
-					$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					$sql = "SELECT `address`.`id`, `address`.`street1` FROM `address` WHERE `address`.`id` IN (SELECT `customer_address`.`address_fk` FROM `customer_address` WHERE `customer_address`.`customer_fk` = ?) ORDER BY `address`.`street1`";
-					$q = $pdo->prepare($sql);
-					$q->execute(array($_SESSION['id']));
-					$address = $q->fetchAll(PDO::FETCH_ASSOC);
-					echo "<select method='POST' name='address_fk'>";
-					foreach ($address as $row) {
-					 echo "<option value='" . $row['id'] . "'>" . $row['street1'] . "</option>";
-					}
-					echo "</select>";
-		        ?>
-			<br>
-			<br>
-			<br>
-	        <h4>Select Credit Card</h4>
-			<p>If you need to add a new address, please do so <a href="ccCreate.php">here</a>.</p>
-		    	<?php
-					$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					$sql = "SELECT `credit_card`.`id`, `credit_card`.`card_number` FROM `credit_card` WHERE `credit_card`.`id` IN (SELECT `customer_credit_card`.`creditcard_fk` FROM `customer_credit_card` WHERE `customer_credit_card`.`customer_fk` = ?) ORDER BY `credit_card`.`card_number`";
-					$q = $pdo->prepare($sql);
-					$q->execute(array($_SESSION['id']));
-					$address = $q->fetchAll(PDO::FETCH_ASSOC);
-					echo "<select method='POST' name='creditcard_fk'>";
-					foreach ($address as $row) {
-					 echo "<option value='" . $row['id'] . "'>" . $row['card_number'] . "</option>";
-					}
-					echo "</select>";
-		        ?>
-			<br>
-			<br>
-			<br>
-		    	<?php
-		    		echo '<input type="submit" value="Place Order">';
-		    		echo '</form';
-		    	?>
-	    </div>
- 		<br>
-        <br>
-		<div>
-          <a href="cart.php" input type="submit" value="Cart">Return to Cart</a>
-        </div>
-        <br>
-        <br>
 
-        <br>
-        <br>
-        <br>
-        <br>
+    	<div id="searchResults" class="row">
+        </div>
+
+    	<div id="hidden">
+		    <div class="row">
+		        <h3>Verify Shipping Information</h3>
+		        <h4>Personal Information:</h4>
+					<?php
+						$customer = new customer();
+						$cust = $customer->read($_SESSION['id']);
+					    
+					    echo '<form method="POST" action="placeOrderFunction.php">';
+					    echo '<input type="hidden" name="id" value="' . $cust['id'] . '">';
+					    echo ''.$cust['name'].'';
+					    echo '&nbsp;'; 
+					    echo ''.$cust['last_name'].'';
+					    echo '<br>';
+					    echo ''.$cust['phone_number'].'';
+					    echo '<br>';
+					    echo ''.$cust['email_address'].'';
+					?>
+				<br>
+				<br>
+				<br>
+		      	<h4>Select Shipping Address</h4>
+		      	<p>If you need to add a new address, please do so <a href="addressCreate.php">here</a>.</p>
+			    	<?php
+						$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+						$sql = "SELECT `address`.`id`, `address`.`street1` FROM `address` WHERE `address`.`id` IN (SELECT `customer_address`.`address_fk` FROM `customer_address` WHERE `customer_address`.`customer_fk` = ?) ORDER BY `address`.`street1`";
+						$q = $pdo->prepare($sql);
+						$q->execute(array($_SESSION['id']));
+						$address = $q->fetchAll(PDO::FETCH_ASSOC);
+						echo "<select method='POST' name='address_fk'>";
+						foreach ($address as $row) {
+						 echo "<option value='" . $row['id'] . "'>" . $row['street1'] . "</option>";
+						}
+						echo "</select>";
+			        ?>
+				<br>
+				<br>
+				<br>
+		        <h4>Select Credit Card</h4>
+				<p>If you need to add a new address, please do so <a href="ccCreate.php">here</a>.</p>
+			    	<?php
+						$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+						$sql = "SELECT `credit_card`.`id`, `credit_card`.`card_number` FROM `credit_card` WHERE `credit_card`.`id` IN (SELECT `customer_credit_card`.`creditcard_fk` FROM `customer_credit_card` WHERE `customer_credit_card`.`customer_fk` = ?) ORDER BY `credit_card`.`card_number`";
+						$q = $pdo->prepare($sql);
+						$q->execute(array($_SESSION['id']));
+						$address = $q->fetchAll(PDO::FETCH_ASSOC);
+						echo "<select method='POST' name='creditcard_fk'>";
+						foreach ($address as $row) {
+						 echo "<option value='" . $row['id'] . "'>" . $row['card_number'] . "</option>";
+						}
+						echo "</select>";
+			        ?>
+				<br>
+				<br>
+				<br>
+			    	<?php
+			    		echo '<input type="submit" class="btn btn-success form-actions" value="Place Order">';
+			    		echo '</form';
+			    	?>
+		    </div>
+	 		<br>
+	        <br>
+			<div>
+	          <a href="cart.php" input type="submit" value="Cart">Return to Cart</a>
+	        </div>
+	        <br>
+	        <br>
+
+	        <br>
+	        <br>
+	        <br>
+	        <br>
+	    </div>
     </div> <!-- /container -->
 
   <?php 
