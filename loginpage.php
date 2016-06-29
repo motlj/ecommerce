6ffd1d3150bf9f614731cb86a020bb31d9ab729e
motlj <joshuadamotl@gmail.com>
2016-06-28 22:57:29 -0500

    stlyed login page

--- loginpage.php
+++ loginpage.php
@@ -17,44 +17,50 @@
     </div>
 
     <div id="hidden">
-      <div class="container">
+      <div class="container" id="login"><center>
         <div class="row">
-          <div class="starter-template">
-            <p class="lead">Please enter username and password to login to your account.<br></p>
+          <div class="col-lg-3 col-md-3 col-sm-0 col-xs-0">
           </div>
-        </div>         
-        <div class="control-group controls">
-          <form action="includes/login.php" method="post">
-            <div class="row">
-              <div class="col-lg-3 col-md-3 col-sm-12">
-                <input type="text" name="user_name" placeholder="User Name">
-              </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <div class="starter-template">
+              <p class="lead">Please enter username and password to login to your account.<br></p>
             </div>
-            <br>
-            <div class="row">
-              <div class="col-lg-3 col-md-3 col-sm-12">
-                <input type="password" name="password" placeholder="Password">
+          </div>         
+          <div class="control-group controls">
+            <form action="includes/login.php" method="post">
+              <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                  <input type="text" name="user_name" placeholder="User Name">
+                </div>
               </div>
-            </div>
-            <br>
-            <div class="row">
-              <div class="col-lg-3 col-md-3 col-sm-12">
-                <input type="submit" class="btn btn-success form-actions" value="Login"> 
+              <br>
+              <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                  <input type="password" name="password" placeholder="Password">
+                </div>
               </div>
-            </div>       
-          </form>
+              <br>
+              <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                  <input type="submit" class="btn btn-success form-actions" value="Login"> 
+                </div>
+              </div>       
+            </form>
+          </div>
+          <br>
+          <div>
+          	<p>If you do not have an account please click register below</p>
+          </div>
+          <br>
+          <div>
+            <form action="register.php" method="post">
+              <input type="submit" class="btn btn-success form-actions" value="Register">
+            </form>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-0 col-xs-0">
+          </div>
         </div>
-        <br>
-        <div>
-        	<p>If you do not have an account please click register below</p>
-        </div>
-        <br>
-        <div>
-          <form action="register.php" method="post">
-            <input type="submit" class="btn btn-success form-actions" value="Register">
-          </form>
-        </div>
-      </div>
+      </center></div>
     </div>
 
     <?php require_once('includes/footer.php');?>
