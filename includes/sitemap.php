fb34d104b518747b2a6eba52a3cd640368f365bd
motlj <joshuadamotl@gmail.com>
2016-06-28 23:14:30 -0500

    added more line breaks

--- includes/sitemap.php
+++ includes/sitemap.php
@@ -12,22 +12,21 @@
  <body>
 
     <div class="container"><h4>
-      <br>
-      <br>
+      <br><br>
       <a alt="Homepage" title="Homepage" href="../index.php">Homepage</a>
-      <br>
+      <br><br>
       <a alt="Register Account" title="Register Account" href="../register.php">Registration</a>
-      <br>
+      <br><br>
       <a alt="Login Page" title="Login Page" href="../loginpage.php">Login Page</a>
-      <br>
+      <br><br>
       <a alt="Update User Information" title="Update User Information" href="../update.php">Update User Information</a>
-      <br>
+      <br><br>
       <a alt="Category List" title="Category List" href="../categoryList.php">List of Categories</a>
-      <br>
+      <br><br>
       <a alt="Product List" title="Product List" href="../productList.php">List of Products</a>
-      <br>
+      <br><br>
       <a alt="Cart" title="Cart" href="../cart.php">Cart</a>
-      <br>
+      <br><br>
     </h4></div><!-- /.container -->
 
   </body>
