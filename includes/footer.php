aaeaf5248ddb795e1af26c6b6e07ae3dfb44411b
motlj <joshuadamotl@gmail.com>
2016-06-28 23:10:41 -0500

    changed column values

--- includes/footer.php
+++ includes/footer.php
@@ -1,19 +1,19 @@
 <div class="container">
-  <br><br><hr><br><br>
+  <br><br><hr>
   <div class="row">
 
-    <div class="col-lg-5 col-md-5 col-sm-0 col-xs-0">
+    <div class="col-lg-4 col-md-4 col-sm-0 col-xs-0">
     </div>
 
-    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12">
+    <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
       <a href="includes/sitemap.php"><center class="bold">Sitemap</center></a>
     </div>
 
-    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12">
+    <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
       <center class="bold">&copy;Josh Motl 2016</center>
     </div>
 
-    <div class="col-lg-5 col-md-5 col-sm-0 col-xs-0">
+    <div class="col-lg-4 col-md-4 col-sm-0 col-xs-0">
     </div>
 
   </div>
